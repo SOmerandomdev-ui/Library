1e8624e2920c2acf62713b4b85746c047b10a55f
SOmerandomdev-ui <[200~218329313+SOmerandomdev-ui@users.noreply.github.com~>
2025-08-24 23:51:00 -0400

    Made the menu and added the form and learned how to use the submissions to call the book object function

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 let books = []
 
+//Sets up the book object 
 function book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
+//Defines the different elements
 let Body = document.querySelector("body")
 let Add = document.querySelector(".Add")
 let Overlay = document.querySelector(".overlay")
 let Main = document.querySelector(".Main")
+let Appear = document.querySelectorAll("form, .title, .X")
 let X = document.querySelector(".X")
+let Form = document.querySelector("form")
 
+//This is for when the add button is clicked and the menu is opened up 
 Add.addEventListener("click", () => {
-    Overlay.style.backgroundColor = "rgba(0, 0, 0, 0.4)"; 
+    Overlay.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
     Main.style.background = "white";
     Main.style.boxShadow = "1px 1px 10px 2px black";
-    X.style.opacity = "1";
-    } 
+    Appear.forEach(element => {
+        element.style.opacity = "1";
+    })
+}
 )
+
+//This is for when x is clicked and it exits the menu 
 X.addEventListener("click", () => {
     Overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
     Main.style.background = "none";
     Main.style.boxShadow = "none";
-    X.style.opacity = "0";
-    } 
+    Appear.forEach(element => {
+        element.style.opacity = "0";
+    })
+}
 )
+
+//This is for when a new book entry has to be created 
+
+Form.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+    let Name = document.querySelector("#Name").value
+    let mybook = new book(Name)
+    alert(mybook.title)
+})
+
+
+
+
 
 
 
