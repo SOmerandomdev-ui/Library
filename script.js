5771c02df156a333918c3957575c4d8c47eb45de
SOmerandomdev-ui <[200~218329313+SOmerandomdev-ui@users.noreply.github.com~>
2025-08-27 20:47:13 -0400

    Added code to make the entry appear on the page

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let Main = document.querySelector(".Main")
 let Appear = document.querySelectorAll("form, .title, .X")
 let X = document.querySelector(".X")
 let Form = document.querySelector("form")
+let Grid = document.querySelector(".Grid")
 
 //This is for when the add button is clicked and the menu is opened up 
 Add.addEventListener("click", () => {
@@ -27,26 +28,58 @@ Add.addEventListener("click", () => {
 }
 )
 
-//This is for when x is clicked and it exits the menu 
-X.addEventListener("click", () => {
+//This is for when x or submit is clicked and it exits the menu 
+
+let Exit = () => { 
     Overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
     Main.style.background = "none";
     Main.style.boxShadow = "none";
     Appear.forEach(element => {
-        element.style.opacity = "0";
-    })
+        element.style.opacity = "0";})
 }
+
+X.addEventListener("click", () => {
+    Exit()
+    }
 )
+
 
 //This is for when a new book entry has to be created 
 
 Form.addEventListener("submit", (e) => {
-
     e.preventDefault()
+    let Div = document.createElement("div")
     let Name = document.querySelector("#Name").value
-    let mybook = new book(Name)
-    alert(mybook.title)
+    let Author = document.querySelector("#Author").value
+    let Pages = document.querySelector("#Pages").value
+    let Read = document.querySelector("#State").value
+    let mybook = new book(Name, Author, Pages)
+    books.push(mybook.title)
+    Grid.appendChild(Div)
+    
+    let DivTitle = document.createElement("div")
+    Div.appendChild(DivTitle)
+    DivTitle.textContent = mybook.title;
+
+    let DivContent = document.createElement("div")
+    Div.appendChild(DivContent)
+
+    for (i = 0; i < 3; i++) {
+        let body = document.createElement("p")
+        DivContent.appendChild(body)
+
+        if (i == 0) {body.textContent= ("Author: " + mybook.author)}
+
+        else if (i == 1) {body.textContent = ("Pages: " + mybook.pages)}
+
+        else if (i == 2) {body.textContent = ("Already Read?: " + Read)}
+    }
+
+    Exit()
 })
+
+
+
 
 
 
